0f79d08951bd79f8376424588a3533b6d3b212fd
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-02-14 16:31:43 -0500

    formatted LoanDetails Component

--- src/LoanDetails.jsx
+++ src/LoanDetails.jsx
@@ -11,12 +11,12 @@ class LoanDetails extends React.Component {
         <div>
           <form>
             <h5>Loan Amount</h5>
-            <input type="text" placeholder="$"></input>
+            <input type="text" placeholder="$5000"></input>
             <h5>Loan Term In Years</h5>
-            <input type="text"></input>
+            <input type="text" placeholder="5"></input>
             <p>Or</p>
             <h5>Loan Term In Months</h5>
-            <input type="text"></input>
+            <input type="text" placeholder="60"></input>
             <h5>Interest Rate Per Year</h5>
             <input type="text" placeholder="%"></input>
           </form>
